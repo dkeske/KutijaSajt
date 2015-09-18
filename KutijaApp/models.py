684d76f04dd038e104ed779180a2b3bf2bd2275a
Stefan Vujović <stefanvujovic93@gmail.com>
2015-09-18 21:50:27 +0200

    update modela

--- KutijaApp/models.py
+++ KutijaApp/models.py
@@ -7,18 +7,22 @@ class User_Regular(models.Model):
 	isAdmin = models.BooleanField()
 
 class Location(models.Model):
-	name = models.CharField(max_length=850)
-	description = models.TextField(max_length = 800)
-	locationLink = models.CharField(max_length = 800)
-	owner = models.ForeignKey(User_Regular)
+	name = models.CharField(max_length=850)            # Location name - example "Faculty of Organizational Sciences" 
+	locationImg = models.CharField(max_length =850)    # Location image
+	description = models.TextField(max_length = 800)   # Location description 
+	locationLink = models.CharField(max_length = 800)  # Link to otganization website
+	longitude = models.CharField(max_length =800)	   # Longitude for Google Maps
+	latitude = models.CharField(max_length =800)	   # Latitude for Google Maps
+	owner = models.ForeignKey(User_Regular) 
 
 class Box(models.Model):
-	Box_ID = models.IntegerField(primary_key=True)
-	Capacity = models.IntegerField()
+	Box_ID = models.IntegerField(primary_key=True)    
+	
 	Location = models.ForeignKey(Location)
 
 class Log(models.Model):
 	NumberOfCaps =  models.IntegerField()
 	Timestamp = models.TimeField()
+	isFull = models.BooleanField()
 	Box = models.ForeignKey(Box)
 
